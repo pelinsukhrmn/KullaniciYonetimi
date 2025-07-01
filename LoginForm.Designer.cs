d1c4c9c76f65c5fa506c044c3ef878c9e6c62dc3
Pelinsu Kahraman <pelinsumarinette@gmail.com>
2025-07-01 17:54:08 +0300

    Add files via upload

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace KullaniciYonetimi.Forms
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblUsername = new Label();
+            lblPassword = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            chkShowPassword = new CheckBox();
+            SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(27, 32);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(92, 20);
+            lblUsername.TabIndex = 0;
+            lblUsername.Text = "Kullanıcı Adı";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(27, 69);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(39, 20);
+            lblPassword.TabIndex = 1;
+            lblPassword.Text = "Şifre";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(141, 29);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(125, 27);
+            txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(141, 69);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(125, 27);
+            txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(27, 150);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Giriş Yap";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // chkShowPassword
+            // 
+            chkShowPassword.AutoSize = true;
+            chkShowPassword.Location = new Point(27, 108);
+            chkShowPassword.Name = "chkShowPassword";
+            chkShowPassword.Size = new Size(125, 24);
+            chkShowPassword.TabIndex = 5;
+            chkShowPassword.Text = "Şifremi Göster";
+            chkShowPassword.UseVisualStyleBackColor = true;
+            chkShowPassword.CheckedChanged += chkShowPassword_CheckedChanged;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(298, 257);
+            Controls.Add(chkShowPassword);
+            Controls.Add(lblPassword);
+            Controls.Add(lblUsername);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblUsername;
+        private Label lblPassword;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private CheckBox chkShowPassword;
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KullaniciYonetimi.Forms
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+
+            InitializeComponent();
+        }
+        private List<Kullanici> kullaniciListesi = new List<Kullanici>
+        {
+            new Kullanici { KullaniciAdi = "admin", Sifre = "1234", Rol = "Admin" },
+            new Kullanici { KullaniciAdi = "kullanici", Sifre = "5678", Rol = "User" }
+        };
+        private void Yonlendir(string rol)
+        {
+            this.Hide();
+
+            Form hedefForm = rol switch
+            {
+                "Admin" => new AdminForm(),
+                "User" => new UserForm(),
+                _ => null
+            };
+
+            if (hedefForm != null)
+            {
+                hedefForm.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Geçersiz rol: " + rol);
+            }
+
+            this.Show();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string kullaniciAdi = txtUsername.Text.Trim();
+            string sifre = txtPassword.Text;
+
+            // Kullanıcıyı listede ara
+            Kullanici girisYapan = kullaniciListesi
+                .FirstOrDefault(k => k.KullaniciAdi == kullaniciAdi && k.Sifre == sifre);
+
+            if (girisYapan != null)
+            {
+                Yonlendir(girisYapan.Rol);
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre hatalı.");
+            }
+        }
+
+        private void chkShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkShowPassword.Checked)
+            {
+                txtPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+    }
+}
